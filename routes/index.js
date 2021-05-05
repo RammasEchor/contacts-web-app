e8d877cae02d99e64da8190cb3093cce7849a0b2
Luis Nieto <luis.nieto.pala@gmail.com>
2021-05-05 13:18:38 -0500

    Testing heroku db

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Contacts App' });
 });
 
-router.post('/db', function (req, res, next) {
+router.post('/db', async function (req, res, next) {
   console.log(req.body);
   var name = req.body.newContact.name;
   var lastName = req.body.newContact.lastName;
@@ -16,19 +23,11 @@ router.post('/db', function (req, res, next) {
   var phone = req.body.newContact.phone;
   var email = req.body.newContact.email;
 
-  const client = new Client({
-    user: 'luis.nieto',
-    host: 'localhost',
-    database: 'contacts',
-    port: 5432,
-  });
-
-  client.connect()
+  const client = await pool.connect();
   var queryString = `INSERT INTO contacts(userId, name, lastName, company, phone, email) 
                         VALUES ('${0}', '${name}', '${lastName}', '${company}', '${phone}', '${email}')`;
 
-  console.log(queryString);
-  query = client.query(queryString, (err, response) => {
+  client.query(queryString, (err, response) => {
     if (err) {
       console.log(err);
       res.sendStatus(500);
@@ -40,16 +39,9 @@ router.post('/db', function (req, res, next) {
 
 });
 
-router.get('/db', function (req, res, next) {
+router.get('/db', async function (req, res, next) {
   console.log(req.body);
-  const client = new Client({
-    user: 'luis.nieto',
-    host: 'localhost',
-    database: 'contacts',
-    port: 5432,
-  });
-
-  client.connect()
+  const client = await pool.connect();
   var queryString = `SELECT * FROM contacts`;
   console.log(queryString);
   query = client.query(queryString, (err, response) => {
