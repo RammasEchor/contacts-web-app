e0a56366e5ff943795a7ea5752c8084bc94e6da5
Luis Nieto <luis.nieto.pala@gmail.com>
2021-05-06 21:35:12 -0500

    Debug

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ router.get('/', function (req, res, next) {
 router.post('/db', async function (req, res, next) {
   console.log(req.body);
 
-  var newContact = req.body('newContact');
-  console.log( 'newContact' );
+  var newContact = req.body.newContact;
+  console.log(newContact);
 
   var name = newContact.name
   var lastName = newContact.lastName;
@@ -26,29 +26,29 @@ router.post('/db', async function (req, res, next) {
   var phone = newContact.phone;
   var email = newContact.email;
 
-  if( !name || /\d/.test(name) )   {
+  if (!name || /\d/.test(name)) {
     res.status(500).send(`Error in name: ${name}`);
-    return ;
+    return;
   }
 
-  if( !lastName || /\d/.test(lastName) )   {
+  if (!lastName || /\d/.test(lastName)) {
     res.status(500).send(`Error in lastName: ${lastName}`);
-    return ;
+    return;
   }
 
-  if( company && !(/^[A-Za-z0-9\s]+$/.test(company)) )    {
+  if (company && !(/^[A-Za-z0-9\s]+$/.test(company))) {
     res.status(500).send(`Error in company: ${company}`);
-    return ;
+    return;
   }
 
-  if( phone && !(/^[0-9]+$/.test(phone)) )    {
+  if (phone && !(/^[0-9]+$/.test(phone))) {
     res.status(500).send(`Error in phone: ${phone}`);
-    return ;
+    return;
   }
 
-  if( !(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) ) {
+  if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))) {
     res.status(500).send(`Error in email: ${email}`);
-    return ;
+    return;
   }
 
   try {
@@ -56,7 +56,7 @@ router.post('/db', async function (req, res, next) {
     var queryString = `INSERT INTO contacts(userId, name, lastName, company, phone, email) 
                         VALUES ('${0}', '${name}', '${lastName}', '${company}', '${phone}', '${email}')`;
     const result = await client.query(queryString);
-    console.log( queryString );
+    console.log(queryString);
     const results = { 'results': (result) ? result.rows : null };
     res.json({ rows: results });
     client.release();
@@ -84,7 +84,7 @@ router.get('/db', async function (req, res, next) {
 router.patch('/db', async function (req, res, next) {
   console.log(req.body);
 
-  if( !req.body.updatedContact.delete ) {
+  if (!req.body.updatedContact.delete) {
     var name = req.body.updatedContact.name;
     var lastName = req.body.updatedContact.lastName;
     var company = req.body.updatedContact.company;
@@ -92,48 +92,48 @@ router.patch('/db', async function (req, res, next) {
     var email = req.body.updatedContact.email;
     var originalEmail = req.body.updatedContact.originalEmail;
 
-    var contactHasError = false ;
+    var contactHasError = false;
 
-    if( !name || /\d/.test(name) )   {
+    if (!name || /\d/.test(name)) {
       var errorMsg = 'Name required and may not have numbers'
-      document.getElementById('errorTextName').innerText = errorMsg ;
-      contactHasError = true ;
+      document.getElementById('errorTextName').innerText = errorMsg;
+      contactHasError = true;
     }
-  
-    if( !lastName || /\d/.test(lastName) )   {
-        var errorMsg = 'Last Name required and may not have numbers' ;
-        document.getElementById('errorTextLastName').innerText = errorMsg ;
-        contactHasError = true ;
+
+    if (!lastName || /\d/.test(lastName)) {
+      var errorMsg = 'Last Name required and may not have numbers';
+      document.getElementById('errorTextLastName').innerText = errorMsg;
+      contactHasError = true;
     }
-  
-    if( company && !(/^[A-Za-z0-9\s]+$/.test(company)) )    {
-        var errorMsg = 'Company may be empty, but may not have symbols' ;
-        document.getElementById('errorTextCompany').innerText = errorMsg ;
-        contactHasError = true ;
+
+    if (company && !(/^[A-Za-z0-9\s]+$/.test(company))) {
+      var errorMsg = 'Company may be empty, but may not have symbols';
+      document.getElementById('errorTextCompany').innerText = errorMsg;
+      contactHasError = true;
     }
-  
-    if( phone && !(/^[0-9]+$/.test(phone)) )    {
-        var errorMsg = 'Phone may be empty, or only be numbers' ;
-        document.getElementById('errorTextPhone').innerText = errorMsg ;
-        contactHasError = true ;
+
+    if (phone && !(/^[0-9]+$/.test(phone))) {
+      var errorMsg = 'Phone may be empty, or only be numbers';
+      document.getElementById('errorTextPhone').innerText = errorMsg;
+      contactHasError = true;
     }
-  
-    if( !(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) ) {
-        var errorMsg = 'Is not a valid email address' ;
-        document.getElementById('errorTextEmail').innerText = errorMsg ;
-        contactHasError = true ;
+
+    if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))) {
+      var errorMsg = 'Is not a valid email address';
+      document.getElementById('errorTextEmail').innerText = errorMsg;
+      contactHasError = true;
     }
-  
-    if( contactHasError )   {
-        console.log('Error in contact data.');
-        res.status(500).send('Ha; try again.');
+
+    if (contactHasError) {
+      console.log('Error in contact data.');
+      res.status(500).send('Ha; try again.');
     }
 
     var query = `UPDATE contacts SET (name,lastname,company,phone,email) =
                   ('${name}','${lastName}','${company}','${phone}','${email}')
                   WHERE email = '${originalEmail}'`;
   }
-  else  {
+  else {
     var mail = req.body.updatedContact.mail;
     var query = `DELETE FROM contacts WHERE email = '${mail}'`;
   }
